7688e525942ecb4abf331dbb0456dbf58cf6ab17
dinesh-rayala <dinesh.rayala@gmail.com>
2022-08-15 15:08:54 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -37,7 +37,6 @@ class Home extends Component {
 
     return (
       <ul className="teams-list">
-        {/* FIX6: The list of team cards should be rendered using Array.map() method */}
         {teamsData.map(team => (
           <TeamCard teamDetails={team} key={team.id} />
         ))}
@@ -46,7 +45,6 @@ class Home extends Component {
   }
 
   renderLoader = () => (
-    // FIX7: For the purpose of testing here testid attribute should be added with the value "loader"
     <div testid="loader" className="loader-container">
       <Loader type="Oval" color="#ffffff" height={50} />
     </div>
